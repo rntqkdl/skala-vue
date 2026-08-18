bcf9280444b5cb3de0730453ffff6bdcaae7ac81
안성민 <tjdals2299@gmail.com>
2026-08-18 17:28:15 +0900

    샘플 4,5 완료

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 import SampleOne from './components/practices/SampleOne.vue'
 import SampleTwo from './components/practices/SampleTwo.vue'
 import SampleThree from './components/practices/SampleThree.vue'
+import SampleFour from './components/practices/Samplefour.vue'
+import SampleFive from './components/practices/SampleFive.vue'
 </script>
 
 <template>
@@ -9,4 +11,6 @@ import SampleThree from './components/practices/SampleThree.vue'
   <SampleOne />
   <SampleTwo />
   <SampleThree />
+  <SampleFour />
+  <SampleFive />
 </template>
