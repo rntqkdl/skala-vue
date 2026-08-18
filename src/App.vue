85ecca361d999558dec3502029063f659f2cc36e
안성민 <tjdals2299@gmail.com>
2026-08-18 17:36:35 +0900

    샘플 8  완료

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import SampleFour from './components/practices/Samplefour.vue'
 import SampleFive from './components/practices/SampleFive.vue'
 import SampleSix from './components/practices/SampleSix.vue'
 import SampleSeven from './components/practices/SampleSeven.vue'
+import SampleEight from './components/practices/SampleEight.vue'
 </script>
 
 <template>
@@ -17,4 +18,5 @@ import SampleSeven from './components/practices/SampleSeven.vue'
   <SampleFive />
   <SampleSix />
   <SampleSeven />
+  <SampleEight />
 </template>
